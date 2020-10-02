25f51c429ddca4c6812c15997d2f82e8b4192132
Drew Ross <mail.drew.ross@gmail.com>
2020-10-02 08:43:05 -0700

    Complete ex1. Test OK.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = None
+    wd = {}
 
-    return None
+    for i, item in enumerate(weights):
+        if item not in wd:
+            wd[item] = []
+        wd[item].append(i)
+
+    if length > 1:
+        for i, item in enumerate(weights):
+            if limit - item in wd:
+                if limit - item == item and len(wd[item]) > 1:
+                    i = wd[item][0]
+                    i2 = wd[item][1]
+                else:
+                    i2 = wd[limit - item][0]
+                result = [max(i, i2), min(i, i2)]
+                break
+
+    return result
